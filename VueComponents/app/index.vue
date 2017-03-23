5586c557206c22c4c0add439bacdc091c00dd147
CaoXuDong <caoxudong@smzdm.com>
2017-03-24 00:34:31 +0800

    it works

---
+++ VueComponents/app/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="test2">
+	yes, you did it!
+	{{msg}}
+	</div>
+
+</template>
+
+<script>
+export default{
+	data(){
+		return {
+			msg: 'shit'
+		}
+	}
+}
+</script>
+<style>
+body{
+	background: green;
+}
+</style>
--- VueComponents/webpack.config.js
+++ VueComponents/webpack.config.js
@@ -1,9 +1,79 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin'); 
+
+// 常用配置,项目较小不抽出了
+var ROOT_PATH = path.resolve(__dirname);//根路径
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+
 module.exports = {
-	entry: 'app.js',
-	output: 'bundle.js',
-	module: {
-		loader:{
-			
+	entry: {
+		app: path.resolve(APP_PATH, 'app.js')
+	},
+	output: {
+		path: BUILD_PATH,
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.vue'],
+		fallback: [path.join(__dirname, './node_modules')],
+		alias: {
+			vue: 'vue/dist/vue.js'
 		}
-	}
+	},
+	//开启 dev source map
+	devtool: 'eval-source-map',
+	//开启 dev source map
+	devServer: {
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		progress: true
+	},
+	module: {
+		loaders:[
+			{
+				test: /\.vue$/,
+				loader: 'vue'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				include: ROOT_PATH,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css/,
+				loader: 'style'
+			},
+			{
+				test: /\.css/,
+				loader: 'css'
+			},
+			{
+				test: /\.html$/,
+				loader: 'vue-html'
+			},
+			{
+        		test: /vux.src.*?js$/,
+        		loader: 'babel'
+      		}
+		]
+	},
+	vue:{
+		loaders: {
+			test: /\.vue$/,
+			loaders: 'vue-style'
+		}
+	},
+	plugins:[
+		new HtmlwebpackPlugin({
+			title: 'Vue component test',
+			filename: 'dist.html',
+			template: 'app/index.html',
+			inject: true
+		})
+	]
 }
